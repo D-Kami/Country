f61c6c52d4add8fa99c0b54851b79c0228051cc3
D-Kami <k.duch9219@gmail.com>
2023-04-28 22:00:15 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        HashMap <String, String> kraj_stolica = new HashMap<>();
+
+        System.out.println("Podaj kraj i stolice (znak '/' konczy)");
+
+        String[] input;
+
+        do {
+            input = scanner.nextLine().split(" ");
+            if (input.length == 2) {
+                kraj_stolica.put(input[0], input[1]);
+            }
+            while (!input[0].equals("/"));
+
+            System.out.println("Podaj kraj aby wyswietlic stolice");
+            String nameToFind = scanner.nextLine();
+            if (kraj_stolica.containsKey(nameToFind)) {
+                System.out.println("Stolica" + nameToFind + " to " + kraj_stolica);
+                else{
+                    System.out.println("nie ma stolicy dla : " + nameToFind);
+                }
+            }
+        }
+
+
+    }
+}
