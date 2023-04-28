1daa3ccdadbb7a4d106c6b3e6629cae1803432e0
D-Kami <k.duch9219@gmail.com>
2023-04-28 22:55:36 +0200

    poprawione

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        HashMap <String, String> kraj_stolica = new HashMap<>();
+        HashMap<String, String> kraj_stolica = new HashMap<String, String>();
 
 
         System.out.println("Podaj kraj i stolice (znak '/' konczy)");
@@ -13,19 +13,19 @@ public class Main {
 
         do {
             input = scanner.nextLine().split(" ");
-            if (input.length == 2) {
+            if (input.length == 2)
                 kraj_stolica.put(input[0], input[1]);
-            }
-            while (!input[0].equals("/"));
-
-            System.out.println("Podaj kraj aby wyswietlic stolice");
-            String nameToFind = scanner.nextLine();
-            if (kraj_stolica.containsKey(nameToFind)) {
-                System.out.println("Stolica" + nameToFind + " to " + kraj_stolica);
-            }else
-                    System.out.println("nie ma stolicy dla : " + nameToFind);
-
-
         }
+            while (!input[0].equals("/")) ;
+
+        System.out.println("Podaj kraj aby wyswietlic stolice");
+        String nameToFind = scanner.nextLine();
+        if (kraj_stolica.containsKey(nameToFind)) {
+            System.out.println("Panstwo: " + nameToFind + " Stolica: " + kraj_stolica);
+        } else
+            System.out.println("nie ma stolicy dla : " + nameToFind);
+
+
     }
-}
+    }
+
